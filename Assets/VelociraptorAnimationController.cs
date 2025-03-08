09f7268b016d3a23ba4175af380ea2010e0513e7
zm-f21 <zynabfayyad21@gmail.com>
2025-03-07 20:56:02 -0800

    Raptor Animator Setup and Idle Trigger
    Initial trigger for the Idle animation has been set up using the keystroke "i" for now. IdleTrigger created in the Animator Window for the Raptor FBX object. Everything happening in the scene called RaptorScene found in assets. In the Animator Window, the idle animation is found and the IdleTrigger can be found in the parameters tab.

---
+++ Assets/VelociraptorAnimationController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaptorAnimationTest : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        // Try to find the Animator on the object or its children
+        animator = GetComponentInChildren<Animator>();
+
+        if (animator == null)
+        {
+            Debug.LogError("Animator NOT found! Make sure the Velociraptor has one.");
+        }
+        else
+        {
+            Debug.Log("Animator found: " + animator.gameObject.name);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("IdleTrigger"); // Trigger animation
+                Debug.Log("Idle animation triggered!");
+            }
+            else
+            {
+                Debug.LogError("Animator is missing!");
+            }
+        }
+    }
+}
